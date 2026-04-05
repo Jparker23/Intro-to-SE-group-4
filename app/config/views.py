e0946cd271590ebd097ac569ce95e68e1111e47e
nomad01948 <madeegatlin16@gmail.com>
2026-04-05 17:31:57 -0500

    fix error pages to show

--- app/config/views.py
+++ app/config/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def custom_404(request, exception):
-    return render(request, "404.html", status=404)
+    return render(request, "generic/404.html", status=404)
 
 def custom_403(request, exception):
-    return render(request, "403.html", status=403)
+    return render(request, "generic/403.html", status=403)
 
 def custom_500(request):
-    return render(request, "500.html", status=500)
+    return render(request, "generic/500.html", status=500)
